92b6040b32a542f675b588d1ed781fddf6b004c4
Popke <popek069@gmail.com>
2025-01-15 23:58:24 +0100

    provider abstract class

--- car_rent.Server/DataProvider/CarRentalDataProvider.cs
+++ car_rent.Server/DataProvider/CarRentalDataProvider.cs
@@ -1,6 +1,38 @@
-﻿namespace car_rent.Server.DataProvider
+﻿using car_rent_api2.Server.Database;
+using car_rent.Server.DTOs;
+using car_rent.Server.Model;
+
+namespace car_rent.Server.DataProvider
 {
-    public class CarRentalDataProvider
+    public abstract class CarRentalDataProvider : ICarRentalDataProvider
     {
+        public abstract Task<List<OfferFromApi>> GetOfferToDisplays(DateTime startDate, DateTime endDate,
+            string search_brand, string search_model,
+            string clientId = "", string email = "");
+
+        public abstract Task<OfferFromApi> GetOneOfferFromApi(string offerId);
+
+        public abstract Task<RentInfoFromApi?> RentCar(string offerId, ApplicationUser user, string clientId);
+
+        public abstract Task<CarDetailsToDisplay> GetCarDetailsToDisplay(string offerId);
+
+        public abstract Task<bool> ReturnCar(string rentId);
+
+        public abstract string GetProviderName();
+
+        public string AddProviderName(string offerId)
+        {
+            return offerId + "*" + GetProviderName();
+        }
+
+        public string RemoveProviderName(string offerIdPlusProvider)
+        {
+            return offerIdPlusProvider.Split("*")[0];
+        }
+
+        public bool CheckIfMyOffer(string offerIdPlusProvider)
+        {
+            return offerIdPlusProvider.Contains(GetProviderName());
+        }
     }
 }
