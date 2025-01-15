2f1031ed41ae63d753d290568a6183c008980d65
pietraldo <pietrzakjakub4321@gmail.com>
2025-01-16 00:23:40 +0100

    fixed bug

--- car_rent.Server/Model/Company.cs
+++ car_rent.Server/Model/Company.cs
@@ -1,9 +1,12 @@
-﻿namespace car_rent_api2.Server.Database
+﻿using System.Text.Json.Serialization;
+
+namespace car_rent_api2.Server.Database
 {
     public class Company
     {
         public Guid Company_ID { get; set; }
         public string Name { get; set; }
+        [JsonIgnore]
         public List<Rent> Rents { get; set; }
     }
 }
